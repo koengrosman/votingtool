b2680f0accd2f174975b98ff20c2fd9728ca869a
Koen Grosman <koengrosman@hotmail.com>
2016-03-02 17:30:01 +0100

    sass is added

--- index.js
+++ index.js
@@ -1,13 +1,40 @@
 var express = require('express');
 var app = express();
 var sassMiddleware = require('node-sass-middleware');
-
+var Sequelize = require('sequelize');
+var bodyParser = require('body-parser');
 var srcPath = __dirname + '/sass/';
 var destPath = __dirname + '/public/styles';
 var path = require('path');
-
 var serveStatic = require('serve-static')
 
+//middleware for using bodyparser
+app.use(bodyParser.urlencoded({
+	extended: true
+}));
+
+//middleware for using sequalize
+var sequelize = new Sequelize('feedbackapp', 'Koen', null, {
+	host: 'localhost',
+	dialect: 'postgres',
+	define: {
+		timestamps: false
+	}
+});
+
+//this defines the new table newidea in sequalize
+var idea = sequelize.define('idea', {
+	title: Sequelize.STRING,
+	description: Sequelize.TEXT,
+	email: Sequelize.STRING
+});
+
+//middleware for using serve static
+app.use('/',
+  serveStatic('./public', {})
+);
+
+//middleware for creating css files from cass files
 app.use('/styles', sassMiddleware({
   src: srcPath,
   dest: path.join(__dirname, 'public/styles'),
@@ -15,15 +42,24 @@ app.use('/styles', sassMiddleware({
   outputStyle: 'expanded'
 }));
 
+//set the default view engine
 app.set('view engine', 'jade');
 
 app.get('/', function (req, res) {
   res.render('index');
 });
 
-app.use('/',
-  serveStatic('./public', {})
-);
+app.get('/postfeature', function (req, res) {
+  res.render('postfeature');
+});
+
+app.post('/createidea', function (req, res) {
+  res.render('postfeature');
+});
+
+app.get('/koen', function (req, res) {
+  res.render('index');
+});
 
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
