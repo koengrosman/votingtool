4b63c258e032ebe79bfed578e83126395a2bb04d
Koen Grosman <koengrosman@hotmail.com>
2016-03-03 10:47:02 +0100

    post idea is done

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ var app = express();
 var sassMiddleware = require('node-sass-middleware');
 var Sequelize = require('sequelize');
 var bodyParser = require('body-parser');
+var pg = require('pg');
 var srcPath = __dirname + '/sass/';
 var destPath = __dirname + '/public/styles';
 var path = require('path');
@@ -22,13 +23,6 @@ var sequelize = new Sequelize('feedbackapp', 'Koen', null, {
 	}
 });
 
-//this defines the new table newidea in sequalize
-var idea = sequelize.define('idea', {
-	title: Sequelize.STRING,
-	description: Sequelize.TEXT,
-	email: Sequelize.STRING
-});
-
 //middleware for using serve static
 app.use('/',
   serveStatic('./public', {})
@@ -42,25 +36,55 @@ app.use('/styles', sassMiddleware({
   outputStyle: 'expanded'
 }));
 
+//this defines the new table newidea in sequalize
+var Idea = sequelize.define('idea', {
+	title: Sequelize.STRING,
+	categories: Sequelize.STRING,
+	description: Sequelize.TEXT,
+	email: Sequelize.STRING
+});
+
 //set the default view engine
 app.set('view engine', 'jade');
 
-app.get('/', function (req, res) {
-  res.render('index');
+app.get('/allideas', function (req, res) {
+	Idea.findAll().then(function(posts) {
+		var data = posts.map(function(post) {
+			return {
+				title: post.dataValues.title,
+				description: post.dataValues.description,
+				email: post.dataValues.email,
+			};
+		});
+		res.render('index', {
+			data: data,
+		});
+	});
 });
 
 app.get('/postfeature', function (req, res) {
   res.render('postfeature');
 });
 
-app.post('/createidea', function (req, res) {
-  res.render('postfeature');
+
+app.post('/createidea', function (request, response) {
+	console.log(request.body);
+	Idea.create({
+			title: request.body.title,
+			categories: request.body.categories,
+			description: request.body.description,
+			email: request.body.email
+	}).then(function(idea) {
+		response.redirect('/postfeature');
+	});
 });
 
 app.get('/koen', function (req, res) {
   res.render('index');
 });
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
+sequelize.sync().then(function() {
+	var server = app.listen(3000, function() {
+		console.log('Example app listening on port: ' + server.address().port);
+	});
 });
